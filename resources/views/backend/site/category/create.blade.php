f9917b39bab8cdc41fa33bcfe8590cdc8bd08db1
example1 <32572574+bestshine@users.noreply.github.com>
2020-09-23 19:55:50 +0530

    update
    update

--- resources/views/backend/site/category/create.blade.php
+++ resources/views/backend/site/category/create.blade.php
@@ -1,6 +1,6 @@
 @extends('backend.layouts.master')
 @section('title')
-    ProductCategory create Page
+    Site Category create Page
 @endsection
 @section('css')
 
@@ -11,7 +11,7 @@
         <div class="">
             <div class="page-title">
                 <div class="title_left">
-                    <h3>ProductCategory Management </h3>
+                    <h3>Site Category Management </h3>
                 </div>
                 <div class="title_right">
                     <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
@@ -39,7 +39,7 @@
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <div class="x_panel">
                         <div class="x_title">
-                            <h2>Create ProductCategory</h2>
+                            <h2>Create Site Category</h2>
                             <ul class="nav navbar-right panel_toolbox">
                                 <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                 </li>
